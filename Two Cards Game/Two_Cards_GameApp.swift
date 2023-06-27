60be7f3e177ffd9001e1f2c4bfece6f532ada5aa
Alena <alenabelovadmvn@gmail.com>
2023-06-27 15:52:11 -0400

    Initial Commit

---
+++ Two Cards Game/Two_Cards_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Two_Cards_GameApp.swift
+//  Two Cards Game
+//
+//  Created by Alena Belova  on 2023-06-27.
+//
+
+import SwiftUI
+
+@main
+struct Two_Cards_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
